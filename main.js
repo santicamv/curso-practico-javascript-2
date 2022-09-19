88eb67e6e4726180b74f95a15fd1e57e7d38bb85
Santiago Camacho <santiagocamachovalbuena96@gmail.com>
2022-09-19 11:15:13 -0500

    Archivos del proyecto

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const menuEmail = document.querySelector(".navbar-email");
-menuEmail.addEventListener("click",toggleDesktopMenu);
+menuEmail.addEventListener("click",activarMenu);
 
 const desktopMenu = document.querySelector(".desktop-menu");
 
-function toggleDesktopMenu(){
+const inact = document.querySelector(".inactive");
+
+function activarMenu(){
     desktopMenu.classList.toggle("inactive");
 }
+
+desktopMenu.addEventListener("mouseleave",desactivarMenu);
+
+function desactivarMenu(){
+    desktopMenu.classList.toggle("inactive");
+}
+
+
+
+const menuMobileCategorias = document.querySelector(".mobile-menu");
+
+const menuIconoMobile = document.querySelector(".menu");
+
+menuIconoMobile.addEventListener("click",activarMenu2);
+
+function activarMenu2(){
+    menuMobileCategorias.classList.toggle("containerInactive")
+}
+
