dd1ac4f0b1096703e035556e47380dd7697c70ac
Santiago Camacho <santiagocamachovalbuena96@gmail.com>
2022-09-19 13:53:48 -0500

    Tercer commit que incluye el menú de categorías y el menu del carrito de compras funcionando correctamente.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
-menuEmail.addEventListener("click",activarMenu);
+menuEmail.addEventListener("click", activarMenu);
 
 const desktopMenu = document.querySelector(".desktop-menu");
 
 const inact = document.querySelector(".inactive");
 
-function activarMenu(){
+function activarMenu() {
     desktopMenu.classList.toggle("inactive");
 }
 
-desktopMenu.addEventListener("mouseleave",desactivarMenu);
+desktopMenu.addEventListener("mouseleave", desactivarMenu);
 
-function desactivarMenu(){
+function desactivarMenu() {
     desktopMenu.classList.toggle("inactive");
 }
 
@@ -21,9 +21,30 @@ const menuMobileCategorias = document.querySelector(".mobile-menu");
 
 const menuIconoMobile = document.querySelector(".menu");
 
-menuIconoMobile.addEventListener("click",activarMenu2);
+menuIconoMobile.addEventListener("click", activarMenu2);
 
-function activarMenu2(){
-    menuMobileCategorias.classList.toggle("containerInactive")
+function activarMenu2() {
+    let isMenuCarritoInactivo = menuCarrito.classList.contains("containerInactive");
+
+    if(!isMenuCarritoInactivo){
+        menuCarrito.classList.add("containerInactive")
+    }
+    menuMobileCategorias.classList.toggle("containerInactive");
 }
 
+
+
+
+const menuCarrito = document.querySelector(".product-detail");
+const iconoCarrito = document.querySelector(".navbar-shopping-cart");
+iconoCarrito.addEventListener("click", mostrarMenuCarrito);
+
+function mostrarMenuCarrito() {
+    let isMenuCategoriasInactivo = menuMobileCategorias.classList.contains("containerInactive");
+
+    if(!isMenuCategoriasInactivo){
+        menuMobileCategorias.classList.add("containerInactive");
+    }
+
+    menuCarrito.classList.toggle("containerInactive")
+}
